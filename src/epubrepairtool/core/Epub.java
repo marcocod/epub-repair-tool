cdbdde77c74b36b6956c6c7ca4121205821247c2
marcocod <marcocod@gmail.com>
2024-07-03 22:36:08 +0200

    Updated Epub.java class

--- src/epubrepairtool/core/Epub.java
+++ src/epubrepairtool/core/Epub.java
@@ -18,8 +18,6 @@ public class Epub {
     
     private File file;
     
-    private boolean analyzed=false;
-    private boolean fixed=false;
     private EOCD eocd=null;
     private ArrayList<Entry> entryList=null;
     
@@ -28,64 +26,61 @@ public class Epub {
     }
     
     public Epub(String filename){
-        this.file=new File(filename);
+        file=new File(filename);
     }
     
     public File getFile(){
-        return this.file;
+        return file;
     }
     
     public void analyze() throws IOException{
-        RandomAccessFile rafid=new RandomAccessFile(this.file, "r");
+        RandomAccessFile rafid=new RandomAccessFile(file, "r");
 
         long eocdPos=EOCD.locateEOCD(rafid);
         if(eocdPos>=0L){
-            EOCD eocd=EOCD.read(rafid, eocdPos);
-            int totalEntries=eocd.getTotalEntries();
-            entryList=new ArrayList<Entry>();
-            long pos=eocd.getCentralDirectoryOffset();
-            for(int k=0;k<totalEntries;k++){
-                CDFH cdfHeader=CDFH.read(rafid, pos);
-                pos+=cdfHeader.size();
-                LFH lfHeader=LFH.read(rafid,cdfHeader.getRelativeOffset());
-                if(cdfHeader.getFilenameLength()!=lfHeader.getFilenameLength()){
-                    throw new IOException("Filename length doesn't match in headers");
-                }
-                Entry entry=new Entry(lfHeader,cdfHeader);
-                if(entry.mustToBeFixed()){
-                    entryList.add(entry);
+            eocd=EOCD.read(rafid, eocdPos);
+            if(eocd!=null){
+                int totalEntries=eocd.getTotalEntries();
+                entryList=new ArrayList<Entry>();
+                long pos=eocd.getCentralDirectoryOffset();
+                for(int k=0;k<totalEntries;k++){
+                    CDFH cdfHeader=CDFH.read(rafid, pos);
+                    pos+=cdfHeader.size();
+                    LFH lfHeader=LFH.read(rafid,cdfHeader.getRelativeOffset());
+                    if(cdfHeader.getFilenameLength()!=lfHeader.getFilenameLength()){
+                        throw new IOException("Filename length doesn't match in headers");
+                    }
+                    Entry entry=new Entry(lfHeader,cdfHeader);
+                    if(entry.mustToBeFixed()){
+                        entryList.add(entry);
+                    }
                 }
             }
         }
         rafid.close();
-        analyzed=true;
     }
     
     public boolean mustToBeFixed(){
-        if(entryList!=null){
-            for(int k=0;k<entryList.size();k++){
-                if(entryList.get(k).mustToBeFixed()){
-                    return true;
-                }
-            }
-            return false;
+        if(eocd!=null && entryList!=null){
+            return entryList.size()>0;
         }else{
             return false;
         }
     }
     
     public void fix() throws IOException{
-        if(this.analyzed==true && this.fixed==false){
-            RandomAccessFile rafid=new RandomAccessFile(this.file, "rw");
-            for(int k=0;k<entryList.size();k++){
-                Entry entry=entryList.get(k);
-                if(entry.mustToBeFixed()==true){
-                    entry.fix();
-                    entry.getCDFH().writeFilenameOnly(rafid);
+        if(this.mustToBeFixed()==true){
+            if(file.canWrite()){
+                RandomAccessFile rafid=new RandomAccessFile(file, "rw");
+                for(int k=0;k<entryList.size();k++){
+                    Entry entry=entryList.get(k);
+                    if(entry.mustToBeFixed()==true){
+                        entry.fix();
+                        entry.getCDFH().writeFilenameOnly(rafid);
+                    }
                 }
+                rafid.close();
             }
-            rafid.close();
-            fixed=true;
         }
     }
     
